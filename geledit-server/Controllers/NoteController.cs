5e79a50c081f363def289b0dd3c1c2a76e776b5a
wiKapo <wiktor200polanowski@gmail.com>
2023-03-21 09:19:46 +0100

    Added possibility to add guests to notes

--- geledit-server/Controllers/NoteController.cs
+++ geledit-server/Controllers/NoteController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using geledit_server.Dtos;
 using geledit_server.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -75,4 +76,44 @@ public class NoteController : ControllerBase
 
         return newNote.Id;
     }
+
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Note))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpPost("{id}/addGuest")]
+    public async Task<IActionResult> AddGuestToNote([FromRoute] long id, [FromBody] string username)
+    {
+        var note = await _db.Notes.Include(x => x.Owner).FirstOrDefaultAsync(x => x.Id == id);
+        if (note == null)
+        {
+            return NotFound("note is nonexistent");
+        }
+
+        var guest = await _db.Users.FirstOrDefaultAsync(x => x.UserName == username);
+        if (guest == null)
+        {
+            return NotFound("user does not exist");
+        }
+        
+        var userId = _userManager.GetUserId(User);
+        if (note.Owner.UserName != userId)
+        {
+            return Unauthorized("you are not owner of this note");
+        }
+        
+        note.Guests.Add(guest);
+        await _db.SaveChangesAsync();
+        
+        return Ok(new Note
+        {
+            Id = note.Id,
+            Content = note.Content,
+            Title = note.Title,
+            Owner = note.Owner,
+            Guests = note.Guests,
+        });
+    }
+    
+    
 }
