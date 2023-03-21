f98b106a07881ca0ec35758d09f4ea286625362e
infinifen <48841404+infinifen@users.noreply.github.com>
2023-03-21 10:43:15 +0100

    add user querying, FromNote in ReturnNoteDto, user controller

--- geledit-server/Dtos/ReturnNoteDto.cs
+++ geledit-server/Dtos/ReturnNoteDto.cs
@@ -1,3 +1,5 @@
+using geledit_server.Models;
+
 namespace geledit_server.Dtos;
 
 public class ReturnNoteDto
@@ -5,5 +7,16 @@ public class ReturnNoteDto
     public long Id { get; set; }
     public string Owner { get; set; }
     public string Title { get; set; }
-    public string Content { get; set; }
+    public string? Content { get; set; }
+
+    public static ReturnNoteDto FromNote(Note note, bool includeContent)
+    {
+        return new ReturnNoteDto
+        {
+            Content = includeContent ? note.Content : null,
+            Id = note.Id,
+            Owner = note.Owner.UserName!,
+            Title = note.Title
+        };
+    }
 }
