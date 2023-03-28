0fc90d4fa37731aa8f7dda5845191bb63db66f0f
LempekPL <superlempek2004@gmail.com>
2023-03-28 23:21:59 +0200

    try to remove invalid tokens

--- geledit-client/components/Navbar/Navbar.tsx
+++ geledit-client/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ import {
 import {FormControl, MenuItem, Select, SelectChangeEvent} from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import AccountIcon from "@mui/icons-material/AccountCircle";
-import {getCookie} from "cookies-next";
+import {getCookie, removeCookies} from "cookies-next";
 import {useEffect, useState} from "react";
 import type {ChangeTheme} from "types/navbar";
 
@@ -29,7 +29,22 @@ const Navbar = ({changeTheme}: { changeTheme: ChangeTheme }) => {
         setTheme(getCookie("NEXT_THEME") as string || "light");
         let t = getCookie("token");
         if (t !== undefined) {
-            setLoggedIn(true)
+            setLoggedIn(true);
+            (async () => {
+                let res = await fetch("http://localhost:5274/private", {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "Authorization": "Bearer "+t
+                    }
+                });
+                console.log(res)
+                if (res.status === 401) {
+                    setLoggedIn(false);
+                    removeCookies("token")
+                }
+            })();
+
         }
     }, []);
 
